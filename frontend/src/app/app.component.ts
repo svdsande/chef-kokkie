9f8f4d9f6f11a0e670d6c02eeb7354bc18f1dabc
Stijn van de Sande <samvandesande1994@gmail.com>
2025-11-03 16:41:00 +0100

    feat: implement sidenav layout with Angular Material and update styles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
