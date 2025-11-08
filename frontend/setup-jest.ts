ae71717c550ebf831a518d913cc27f242a44add8
Stijn van de Sande <samvandesande1994@gmail.com>
2025-11-08 14:19:38 +0100

    chore: add ng-mocks dependency and update Jest setup for Angular testing

--- frontend/setup-jest.ts
+++ frontend/setup-jest.ts
@@ -1,4 +1,18 @@
+import { ngMocks } from 'ng-mocks';
+import { DefaultTitleStrategy, TitleStrategy } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApplicationModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MockService } from 'ng-mocks';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone/index.mjs';
 import './jest-global-mocks';
 
 setupZoneTestEnv();
+
+ngMocks.autoSpy('jest');
+
+ngMocks.defaultMock(TitleStrategy, () => MockService(DefaultTitleStrategy));
+
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
