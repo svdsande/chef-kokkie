73e0e0827f587eddfd5e20781bb38dc56bc44fd8
Stijn van de Sande <samvandesande1994@gmail.com>
2025-12-22 16:40:16 +0100

    fix: shopping list component test

--- frontend/src/app/components/shopping-list/shopping-list.component.spec.ts
+++ frontend/src/app/components/shopping-list/shopping-list.component.spec.ts
@@ -1,9 +1,17 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 import { ShoppingListComponent } from './shopping-list.component';
+import { MockProvider } from 'ng-mocks';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list-service';
+import { of } from 'rxjs';
 
 describe('ShoppingListComponent', () => {
   let spectator: Spectator<ShoppingListComponent>;
-  const createComponent = createComponentFactory(ShoppingListComponent);
+  const createComponent = createComponentFactory({
+    component: ShoppingListComponent,
+    providers: [MockProvider(ShoppingListService, {
+      getShoppingList: jest.fn().mockReturnValue(of([]))
+    })]
+  });
 
   beforeEach(() => {
     spectator = createComponent();
